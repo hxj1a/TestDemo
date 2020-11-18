44c68e4afb34be4db1d79d6c1c781dba42c71f94
myecilpse.user <myecilpse@bdqn.com>
2020-11-18 15:30:31 +0800

    '修改了乱码问题!'

--- src/com/hxj/hhh/Text.java
+++ src/com/hxj/hhh/Text.java
@@ -2,6 +2,6 @@ package com.hxj.hhh;
 
 public class Text {
 	public static void main(String[] args) {
-		System.out.println("<h1>ÄãºÃ!GitHubÔ¶³Ì¿â</h1>");
+		System.out.println("<h1>ä½ å¥½!GitHubè¿œç¨‹åº“</h1>");
 	}
 }
