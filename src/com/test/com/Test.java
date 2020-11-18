e8a7441506ba66a01510766f52ae85510489120d
myecilpse.user <myecilpse@bdqn.com>
2020-11-18 15:21:55 +0800

    上传到GitHub上修改的文件

--- src/com/test/com/Test.java
+++ src/com/test/com/Test.java
@@ -2,6 +2,7 @@ package com.test.com;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hello!Git你好欢迎您学习GitHub");
+		System.out.println("hello!Git你好欢迎您学习GitHub,");
+		System.out.println("好好学习!天天向上!");
 	}
 }
