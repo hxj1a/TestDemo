773f2e8d5bf1772ec2786c71c61881ad70054530
hxj1a <74338040+hxj1a@users.noreply.github.com>
2020-11-18 15:34:02 +0800

    修改了文件git
    修改.git

--- src/com/hxj/hhh/Text.java
+++ src/com/hxj/hhh/Text.java
@@ -2,6 +2,6 @@ package com.hxj.hhh;
 
 public class Text {
 	public static void main(String[] args) {
-		System.out.println("<h1>你好!GitHub远程库,学习GitHub远程库</h1>");
+		System.out.println("<h1>你好!GitHub远程库,学习GitHub远程库abcdtgfgfg</h1>");		
 	}
 }
